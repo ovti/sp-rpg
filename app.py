8cb1735e0c7dac69b766f672a72020def952d002
ovti <71078808+ovti@users.noreply.github.com>
2023-12-31 00:10:42 +0100

    project setup

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, redirect, url_for 
+from flask import Flask, render_template, redirect, url_for, request 
   
 app = Flask(__name__) 
 app.secret_key = "secret key"
 
-@app.route("/") 
-def home(): 
-    return render_template("hello.html") 
+@app.route("/", methods =["GET", "POST"])
+def home():
+    player_name = None 
+    if request.method == 'POST':
+        player_name = request.form.get('player_name')
+
+    return render_template('home.html', player_name=player_name)
+
+@app.route("/hello")
+def hello():
+    return render_template("hello.html")
+
 
 if __name__ == '__main__':
-    app.run(host='wierzba.wzks.uj.edu.pl', port=5119, debug=True)
+    app.run(host='wierzba.wzks.uj.edu.pl', port=5127, debug=True)
 
