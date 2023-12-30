ccb010742098325f5a6bd64662aeda33a6bb1423
ovti <71078808+ovti@users.noreply.github.com>
2023-12-30 21:33:49 +0100

    setting up the project

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, redirect, url_for 
   
 app = Flask(__name__) 
+app.secret_key = "secret key"
 
-@app.route("/hello") 
+@app.route("/") 
 def home(): 
     return render_template("hello.html") 
+
+if __name__ == '__main__':
+    app.run(host='wierzba.wzks.uj.edu.pl', port=5119, debug=True)
+
