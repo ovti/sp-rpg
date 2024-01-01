06f580574a435d615788a5fbb903660795a6bccf
ovti <71078808+ovti@users.noreply.github.com>
2024-01-01 20:30:37 +0100

    progres

--- app.py
+++ app.py
@@ -6,19 +6,31 @@ app = Flask(__name__)
 app.secret_key = 'some_secret'
 games = {}
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/')
+def start():
     if 'key' not in session:
         session['key'] = str(uuid.uuid4())
-    if session['key'] not in games:
-        games[session['key']] = Game()    
+    
+    # if session['key'] not in games:
+    #     games[session['key']] = Game()    
+        
+    return render_template('start.html')
+    
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    return 'hello';
+
+
+@app.route('/battle', methods=['GET', 'POST'])
+def battle():
     
     if request.method == 'POST':
         games[session['key']].player_attack()
         games[session['key']].ai_attack()
-        return redirect(url_for('index'))  # Redirect after POST
+        return redirect(url_for('battle'))  # Redirect after POST
         
-    return render_template('index.html', status=games[session['key']].get_player_status(), dead=games[session['key']].is_dead())
+    return render_template('battle.html', status=games[session['key']].get_player_status(), dead=games[session['key']].is_dead())
 
 if __name__ == '__main__':
     
